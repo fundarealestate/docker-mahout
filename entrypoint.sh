88f387c91fb7fc52314ca7892f084e989972c6c7
Joao Antao <joaoantao@funda.nl>
2018-06-06 12:18:07 +0200

    Changed cat to cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Start SSH server (needed by Hadoop)
 service ssh start
 # Setup SSH keys to prevent Hadoop from asking for password
-cat /dev/zero | ssh-keygen -q -N ""
+cmd /dev/zero | ssh-keygen -q -N ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
 ssh-keyscan -H localhost >> ~/.ssh/known_hosts
